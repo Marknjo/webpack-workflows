4645fe1c8d41569c59b6bee5befa235bb1add8ed
Marknjo <marknjo7@gmail.com>
2021-04-05 14:56:52 +0300

    Adds Support for .mp4 and .webm files to the project

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,56 +3,66 @@ const path = require("path");
 const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
-	entry: {
-		main: "./src/index.js",
-		vendor: "./src/scripts/vendor/vendor.js",
-	},
-	output: {
-		path: path.resolve(__dirname, "../public/"),
-		publicPath: "",
-	},
-	plugins: [
-		new htmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-		new DashboardPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.html$/,
-				loader: "html-loader",
-			},
-			{
-				test: /\.(svg|png|jpg|gif)$/,
-				use: {
-					loader: "file-loader",
-					options: {
-						name: "[name].[hash].[ext]",
-						outputPath: "assests/images",
-					},
-				},
-			},
-			{
-				// Apply rule for fonts files
-				test: /\.(woff|woff2|ttf|otf|eot)$/,
-				use: [
-					{
-						// Using file-loader too
-						loader: "file-loader",
-						options: {
-							outputPath: "assests/fonts",
-						},
-					},
-				],
-			},
-		],
-	},
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/scripts/vendor/vendor.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "../public/"),
+    publicPath: "",
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new DashboardPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "assests/images",
+          },
+        },
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "assests/videos",
+          },
+        },
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: "file-loader",
+            options: {
+              outputPath: "assests/fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
