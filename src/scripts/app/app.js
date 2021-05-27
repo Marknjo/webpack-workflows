9369efae280681c51a9c9ba61e79c0af8d15fd6f
Marknjo <marknjo7@gmail.com>
2021-05-27 11:30:57 +0300

    Adding a custom console separator for console outputs

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,3 +1,23 @@
 import "./utils/utils";
+/**
+ * Creates a separator markers with a title on the console
+ * Pretty console visual separator
+ *
+ * @param {String} title Optional, default equal to (./END)
+ * @param {Number} separatorLen Optional, default equal to (20)
+ * @return {Void} logs the separator to the console
+ * @todo add custom sepator indicator. Current default is "-"
+ * @author Mark Njoroge
+ */
+const consoleSeparator = (title = "./END", separatorLen = 20) => {
+  console.log("\n\n");
+  console.log(
+    `${"-".repeat(separatorLen)}     ${title}      ${"-".repeat(separatorLen)}`
+  );
+  console.log("\n");
+};
 
-console.log("App");
+//
+//   Your Custom Separator
+////////////////////////////////////////
+consoleSeparator();
